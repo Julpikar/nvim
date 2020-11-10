55bd2566c080b633203158a5abc7c000750d5743
p0w0p <julpikargupran@gmail.com>
2020-11-10 10:26:49 +0700

    fixing higlight dan balooon ndak bisa

--- lua/linter.lua
+++ lua/linter.lua
@@ -1,7 +1,7 @@
 vim.g.ale_disable_lsp = 1
 vim.g.ale_set_balloons = 1
 vim.g.ale_lint_on_enter = 0
-vim.g.ale_lint_delay = 300
+vim.g.ale_lint_delay = 500
 vim.g.ale_fix_on_save = 1
 vim.g.ale_sign_column_always = 1
 vim.g.ale_sign_error = '✘'
