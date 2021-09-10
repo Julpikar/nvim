c82e6452a2104d4f904531358cc816cb195337dc
unknown <julpikargupran@gmail.com>
2021-09-10 10:36:47 +0700

    fix satusline

--- lua/plugin/galaxyline/init.lua
+++ lua/plugin/galaxyline/init.lua
@@ -140,8 +140,8 @@ local function left_config()
   left[8] = {
     LspStatus = {
       provider = function()
-        local get_lsp_client = function(msg)
-          local msg = msg or "No Active Lsp"
+        local get_lsp_client = function()
+          local msg = "No Active Lsp"
           local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
           local clients = vim.lsp.get_active_clients()
           if next(clients) == nil then
