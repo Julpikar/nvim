d7a7fd804d666b451e86f6755e2c787069596c77
Julpikar <julpikargupran@gmail.com>
2024-08-11 12:44:05 +0700

    fix

--- lua/plugin/nvim-lspconfig.lua
+++ lua/plugin/nvim-lspconfig.lua
@@ -92,6 +92,12 @@ local function server_register()
   lspconfig.clangd.setup({
     on_attach = custom_on_attach,
     capabilities = custom_capabilities,
+    on_new_config = function(new_config, new_cwd)
+      local status, cmake = pcall(require, "cmake-tools")
+      if status then
+        cmake.clangd_on_new_config(new_config)
+      end
+    end,
     autostart = false,
   })
 
